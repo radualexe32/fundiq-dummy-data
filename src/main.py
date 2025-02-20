969f6a84b558cb554f97c76b8c4d4cb45518401d
Radu Alexe <radualexe32@outlook.com>
2025-02-20 13:26:18 -0500

    template setup

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import polars as pl
+import aiomysql
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+from table import Table
+
+load_dotenv()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/table.py
@@ -0,0 +1,12 @@
+import aiomysql
+
+
+class Table:
+    def __init__(self):
+        pass
+
+    def batch_process(self):
+        pass
+
+    def update(self):
+        pass
